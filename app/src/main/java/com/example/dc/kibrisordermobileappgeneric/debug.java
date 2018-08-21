08cc35014308dca8f61550c77846f7a9d58b9cd9
Musa Jahun <msjahun@live.com>
2018-08-21 13:41:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dc/kibrisordermobileappgeneric/debug.java
@@ -0,0 +1,44 @@
+package com.example.dc.kibrisordermobileappgeneric;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class debug extends AppCompatActivity {
+    private static final String TAG = "debugActivity";
+    ListView listViewDebug;
+    ArrayAdapter adapter;
+    String Activities[] ={"No activities yet"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+
+
+        Log.d(TAG, "onCreate: ");
+
+        listViewDebug = (ListView) findViewById(R.id.listViewDebug);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Activities);
+
+        listViewDebug.setAdapter(adapter);
+        listViewDebug.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        startActivity(new Intent(debug.this,debug.class));break;
+
+                }
+
+            }
+        });
+
+    }
+}
