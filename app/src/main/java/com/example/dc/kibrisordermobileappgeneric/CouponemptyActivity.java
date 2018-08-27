09dcb021d76d0beffdca279cbcb921f9cf03682d
Nahyomee <tohree8@gmail.com>
2018-08-27 21:34:12 +0300

    created empty coupon  page

---
+++ app/src/main/java/com/example/dc/kibrisordermobileappgeneric/CouponemptyActivity.java
@@ -0,0 +1,13 @@
+package com.example.dc.kibrisordermobileappgeneric;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CouponemptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_couponempty);
+    }
+}
